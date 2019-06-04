840e64d114a1dbaec4c4ad311ed5f27e3a6a1f0a
kami <kami0314@163.com>
2019-06-04 17:48:54 +0800

    create project

---
+++ ace-sidecar/ace-sidecar-server/src/main/java/com/github/wxiaoqi/security/sidecar/ClassAOP.java
@@ -0,0 +1,33 @@
+package com.github.wxiaoqi.security.sidecar;
+import com.github.wxiaoqi.security.api.vo.log.LogInfo;
+
+import java.util.Date;
+
+/**
+ * test
+ *
+ * @author Venus Zhang
+ * @create 2019-06-01 18:18
+ */
+
+public class ClassAOP {
+
+
+	public static final String STR = "";
+
+	public static void main(String[] args) {
+		LogInfo logInfo = new LogInfo();
+		logInfo.setMenu("");
+		logInfo.setOpt("");
+		logInfo.setUri("");
+		logInfo.setCrtTime(new Date());
+		logInfo.setCrtUser("");
+		logInfo.setCrtName("");
+		logInfo.setCrtHost("");
+		logInfo.setBody("");
+
+
+
+
+	}
+}
